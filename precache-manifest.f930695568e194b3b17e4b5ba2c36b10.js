daf5b08e91a81a054319d81d4c8acadeab4166a4
Cherry Joy Pepito <59734182+GGprog@users.noreply.github.com>
2022-05-26 15:19:16 +0800

    Add files via upload

---
+++ precache-manifest.f930695568e194b3b17e4b5ba2c36b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89f6e1e39986c12ecdad",
+    "url": "/static/css/main.9937e3c7.chunk.css"
+  },
+  {
+    "revision": "89f6e1e39986c12ecdad",
+    "url": "/static/js/main.e0151fe6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3be454d7703791fc014",
+    "url": "/static/js/2.f50e42e3.chunk.js"
+  },
+  {
+    "revision": "2bf82a1521847d7e982ea1cd1cffed26",
+    "url": "/index.html"
+  }
+];
